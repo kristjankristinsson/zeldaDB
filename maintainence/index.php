ac047e9e76ceafc2c5b76ca35a631811f9dda75d
kristjankristinsson <97900321+kristjankristinsson@users.noreply.github.com>
2023-02-08 11:22:02 +0100

    503
    503 php

---
+++ maintainence/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Game Zelda-DB</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+<header>
+        <nav class="main-header">
+            <div class="menu_wrapper">
+                <div class="logo-wrapper">
+                    <a href="../index.php" class="logo">Zelda-DB</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+    <div class="container">
+        <div class="wrapper_maintainable">
+            <h4>Hey!<br>Listen!</h4>
+            <img src="../images/extra_images/navi.png" alt="">
+            <h2>503 Service Unavailable</h2>
+            <p>The site is currently down for <br> maintenence. Please check back later!</p>
+        </div>
+    </div>
+
+    </section>
+    
+</body>
+</html>
