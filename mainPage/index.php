bff8277e63ac20439c5919a0d3c55f4750d68484
kristjankristinsson <97900321+kristjankristinsson@users.noreply.github.com>
2023-02-08 11:21:41 +0100

    mainPage
    Include

---
+++ mainPage/index.php
@@ -0,0 +1,13 @@
+    <?php include "../include/header.php" ?>
+    
+
+    <section class="games_wrapper">
+        <div class="container">
+            <div id="game_list"></div>
+        </div>
+    </section>
+
+    <script src="../js/action.js"></script>
+ 
+</body>
+</html>
